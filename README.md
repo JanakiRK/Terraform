cc7ce934733525b465946cd184237c37c57b9d55
Janaki Radhakrishnan <janaki26794@gmail.com>
2023-04-22 19:47:10 +0100

    Modified access keys and added README commands

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Terraform
 Terrafrom Projects
+
+#Commands used for Terraform are provided below as follows:
--- first_ec2.tf
+++ first_ec2.tf
@@ -2,14 +2,18 @@
 #Configurations regarding to startup /authentication of AWS
 
 provider "aws" {
-    region = "us-west-2"
-    access_key = "AKIA3DKUFMB32Z6HI63N"
-    secret_key = "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" 
+  region     = "us-west-2"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
 }
 
 #Process of creating an EC2 instance
 
-resource "aws_instance" "MyFirstInstance" {
+resource "aws_instance" "myEC2" {
     ami = "ami-009c5f630e96948cb"
     instance_type = "t2.micro"
+
+    tags = {
+        Name = "MyFirstEC2Instance"
+    }
 }
